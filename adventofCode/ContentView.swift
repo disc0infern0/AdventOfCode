182dd6b3bccb1df4c9a399e7c5fbff53545c9497
Andrew <007gnail@gmail.com>
2021-12-12 11:40:04 +0000

    day 1 complete

--- adventofCode/ContentView.swift
+++ adventofCode/ContentView.swift
@@ -1,13 +1,13 @@
 
 import SwiftUI
 
-// Advent of Code blank starting point
+// Advent of Code: Day One parts one and two
 
 struct ContentView: View {
 
     var body: some View {
        let depths = transformPuzzleInput()
-       let answer = increases(depths: depths)
+       let answer = increasesByThrees(depths: depths)
        Text("The answer is \(answer)")
           .padding()
           .frame(minWidth:150, minHeight: 100)
@@ -17,6 +17,7 @@ func transformPuzzleInput() -> [Int] {
    var depths: [Int] = []
    var tmp: String = ""
    for char in puzzleInput {
+//   for char in testInput {
       if char == "\n" {
          depths.append( Int(tmp) ?? 0 )
          tmp=""
@@ -27,6 +28,19 @@ func transformPuzzleInput() -> [Int] {
    return depths
 }
 
+// Part 2 of Day 2
+func increasesByThrees(depths: [Int]) -> Int {
+   var counter = 0
+   var last = depths[0] + depths[1] + depths[2]
+   for i in 1...(depths.count-3) {
+		let next = depths[i]+depths[i+1]+depths[i+2]
+      if next > last { counter += 1 }
+      last = next
+   }
+	return counter
+}
+
+// Part 1 of Day 1
 func increases(depths: [Int]) -> Int {
    var counter = 0
    let initialdepth = depths.max()
@@ -36,9 +50,8 @@ func increases(depths: [Int]) -> Int {
       }
       return j
    }
-	return counter
+   return counter
 }
-
 
 
 struct ContentView_Previews: PreviewProvider {
