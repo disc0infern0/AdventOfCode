2a2320c47e014a5247d953f55908bdbba00fb6a8
Andrew <007gnail@gmail.com>
2021-12-12 09:43:33 +0000

    Initial Commit

---
+++ adventofCode/adventofCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  adventofCodeApp.swift
+//  adventofCode
+//
+//  Created by Andrew Cowley on 12/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct adventofCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
