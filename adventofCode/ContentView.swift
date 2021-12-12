878fcd47ebdee6cc4f91abd1e076effcced32d9b
Andrew <007gnail@gmail.com>
2021-12-12 09:45:42 +0000

    blank

--- adventofCode/ContentView.swift
+++ adventofCode/ContentView.swift
@@ -1,18 +1,16 @@
-//
-//  ContentView.swift
-//  adventofCode
-//
-//  Created by Andrew Cowley on 12/12/2021.
-//
 
 import SwiftUI
 
+// Advent of Code blank starting point
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
     }
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
