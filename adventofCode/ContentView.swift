b05811ecebbe058c95dbb6dbe626de5bbf5294f4
Andrew <007gnail@gmail.com>
2021-12-12 10:58:07 +0000

    day1 done

--- adventofCode/ContentView.swift
+++ adventofCode/ContentView.swift
@@ -4,11 +4,40 @@ import SwiftUI
 // Advent of Code blank starting point
 
 struct ContentView: View {
-    var body: some View {
 
+    var body: some View {
+       let depths = transformPuzzleInput()
+       let answer = increases(depths: depths)
+       Text("The answer is \(answer)")
+          .padding()
+          .frame(minWidth:150, minHeight: 100)
     }
 }
+func transformPuzzleInput() -> [Int] {
+   var depths: [Int] = []
+   var tmp: String = ""
+   for char in puzzleInput {
+      if char == "\n" {
+         depths.append( Int(tmp) ?? 0 )
+         tmp=""
+      } else {
+			tmp = tmp + String(char)
+      }
+   }
+   return depths
+}
 
+func increases(depths: [Int]) -> Int {
+   var counter = 0
+   let initialdepth = depths.max()
+   let _ = depths.reduce(initialdepth) { i,j in
+      if j > i ?? 0 {
+         counter += 1
+      }
+      return j
+   }
+	return counter
+}
 
 
 
