f915ea1ef2c50620c485981f83e688a7d214ca56
Andrew <007gnail@gmail.com>
2021-12-14 12:04:20 +0000

    day3 done

--- adventofCode/ContentView.swift
+++ adventofCode/ContentView.swift
@@ -1,83 +1,153 @@
 
 import SwiftUI
 
-// Advent of Code: Day Two
+// Advent of Code blank starting point
 
-enum Direction: String, CaseIterable {
-   case forward, up, down
-
-   static let allStrings: [String] = Direction.allCases.map { $0.rawValue }
-}
-struct Movement {
-   var direction: Direction
-   var amount: Int
-}
 struct ContentView: View {
-
    var body: some View {
-      let movements = transformPuzzleInput()
-      //       let answer = partOne(after: movements)
-      //       Text("The answer to part One is \(answer.x) x \(answer.y) = \(answer.x * answer.y)")
-      let answer = partTwo(after: movements)
-      Text("The answer to part Two is \(answer.x) x \(answer.y) = \(answer.x * answer.y)")
-         .padding()
-         .frame(minWidth:250, minHeight: 100)
+
+      // Common
+      let parsed = parse(input: puzzleData)
+
+      // Part One
+      let transformed = transform(input: parsed)
+      let gamma = getGamma(input: transformed)
+      let epsilon = getEpsilon(input: gamma)
+      let partOne = String(gamma * epsilon)
+
+      // Part Two
+      let oxygenRating = getOxygenRating(strings: parsed)
+      let carbonRating = getCarbonRating(strings: parsed)
+      let partTwo = String(oxygenRating*carbonRating)
+
+
+      Text("The answer to part one is \(partOne)")
+         .padding(4)
+         .frame(minWidth: 50, maxWidth: .infinity, minHeight: 50 )
+         .textSelection(.enabled)
+      Text("The answer to part two is \(partTwo)")
+         .padding(4)
+         .frame(minWidth: 50, maxWidth: .infinity, minHeight: 50 )
+         .textSelection(.enabled)
    }
 }
-func transformPuzzleInput() -> [Movement] {
-   var answer: [Movement] = []
-   var direction : Direction?
+func getGamma(input: [[Int]]) -> Int {
+   var tmp = ""
+   for column in input {
+      let sum = column.reduce(0,+)
+      let count = column.count
+      tmp += sum >= count/2 ? "1" : "0"
+   }
+   return Int(tmp, radix: 2)!
+}
+
+func getEpsilon(input: Int) -> Int {
+   let data = String(input, radix: 2)
+   var epsilonString = ""
+   for c in data {
+      epsilonString += c=="1" ? "0" : "1"
+   }
+   return Int(epsilonString, radix: 2)!
+}
+// String subscripts
+extension String {
+   subscript(i: Int) -> String {
+      return String(self[index(startIndex, offsetBy: i)])
+   }
+}
+
+// Oxygen Rating
+// given a position, filter the array to show only the lines with the
+// most common digit in that position. if 0 and 1's are both equal, select the 1's
+func getOxygenRating(strings: [String]) -> Int {
+   guard strings.count > 0 else { return -1 }
+   var filteredStrings = strings
+   for digit in 0...filteredStrings[0].count {
+      filteredStrings = o2iterate(digit: digit, strings: filteredStrings)
+      if filteredStrings.count == 1 {
+         return Int(filteredStrings[0], radix: 2)!
+      }
+   }
+   print("o2 rating not found")
+   return -1
+}
+
+func o2iterate(digit: Int, strings: [String]) -> [String] {
+   guard strings.count > 0, digit < strings[0].count else { return ["error"] }
+
+   let column = transform(input: strings)[digit]
+   let sum: Double = Double( column.reduce(0,+) )
+   let midpoint: Double = Double(column.count) / 2
+   let filter = sum >= midpoint ? "1" : "0"
+
+   return strings.filter { string in string[digit] == filter }
+}
+
+// Carbon Rating
+// given a position, filter the array to show only the lines with the
+// least common digit in that position. if 0 and 1's are both equal, select the 0's
+func getCarbonRating(strings: [String]) -> Int {
+   guard strings.count > 0 else { return 3 }
+   var filteredStrings = strings
+   for digit in 0...filteredStrings[0].count {
+      filteredStrings = carbonIterate(digit: digit, strings: filteredStrings)
+      if filteredStrings.count == 1 {
+         return Int(filteredStrings[0], radix: 2)!
+      }
+   }
+   print("carbon rating not found")
+   return 2
+}
+
+func carbonIterate(digit: Int, strings: [String]) -> [String] {
+   guard strings.count > 0 else { return ["error"] }
+
+   let column = transform(input: strings)[digit]
+   let sum: Double = Double( column.reduce(0,+) )
+   let midpoint: Double = Double(column.count) / 2
+   let filter = sum >= midpoint ? "0" : "1"
+
+   return strings.filter { string in string[digit] == filter }
+}
+
+func parse(input: String) -> [String] {
+   var output: [String] = []
    var tmp: String = ""
-   for char in puzzleInput {
-      let c = String(char)
-      if c == " " { continue }
-      if char == "\n" {
-         if let d = direction, let amount = Int(tmp) {
-            answer.append(Movement(direction: d, amount: amount))
-            tmp = ""
-            direction = nil
-            continue
-         }
-      }
-      tmp = tmp + c
-      if (direction==nil) {
-         if let _ = Direction.allStrings.firstIndex(of: tmp) {
-            direction = Direction(rawValue: tmp)
-            tmp = ""
-         }
+
+   for c in input {
+      if c == "\n" {
+         output.append(tmp)
+         tmp = ""
+      } else {
+         tmp += String(c)
       }
    }
-   return answer
+   if tmp != "" {
+      output.append(tmp)
+   }
+   return output
 }
 
-func partOne(after movements: [Movement]) -> (x: Int, y: Int) {
-   var x = 0
-   var y = 0
-   for movement in movements {
-      switch movement.direction {
-         case .up: y = y - movement.amount
-         case .down: y = y + movement.amount
-         case .forward: x = x + movement.amount
-      }
-   }
-   return (x,y)
-}
+func transform(input: [String]) -> [[Int]] {
 
-func partTwo(after movements: [Movement]) -> (x: Int, y: Int) {
-   var forward: Int = 0
-   var depth: Int = 0
-   var aim: Int = 0
-   for movement in movements {
-      switch movement.direction {
-         case .up: aim -= movement.amount
-         case .down: aim += movement.amount
-         case .forward:
-            forward += movement.amount
-            depth += aim * movement.amount
+
+   let digitCount = input[0].count
+   let lineCount = input.count
+   let column = Array(repeating: 0, count: lineCount)
+   var output = Array(repeating: column, count: digitCount)
+
+   var col=0
+   var row=0
+   for line in input {
+      col=0
+      for c in line {
+         output[col][row] = Int(String(c))!
+         col+=1
       }
+      row+=1
    }
-   print(forward*depth)
-   return (forward, depth)
+
+   return output
 }
 
 
