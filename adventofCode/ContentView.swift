20f022629d74653c43468c361f6f21a3c5d8e8bd
Andrew <007gnail@gmail.com>
2021-12-26 00:31:40 +0000

    Day5 Optimised

--- adventofCode/ContentView.swift
+++ adventofCode/ContentView.swift
@@ -1,8 +1,7 @@
 // Advent of Code: Day Five
 import SwiftUI
-import Matrix
 
-var testData = false // set to true to use test data
+var testData = true // set to true to use test data
 
 // use a struct instead of overwriting the matrix value, when called, to a marker value, e.g. -1
 struct Point: Equatable {
@@ -46,14 +45,30 @@ struct ContentView: View {
    }
 }
 
-
-
-
 func partOne(_ lines: [Line]) -> Int {
    var overlapped = 0
    let arraySize = testData ? 10 : 1000
    let row = Array(repeating: 0, count: arraySize)
    var grid = Array(repeating: row, count: arraySize)
+
+   for line in lines where line.from.x == line.to.x || line.from.y == line.to.y {
+      overlapped += countOverlaps(in: line, on: &grid)
+   }
+   print("Part One")
+   printGrid(grid)
+   return overlapped
+}
+
+func printGrid(_ grid: [[Int]]) {
+   print("Final grid")
+   for (counter, row) in grid.enumerated() {
+      print("\(counter+1)\t\t", terminator: "")
+      print(row.reduce("") { j,i in i==0 ? j+"." : j+String(i) })
+   }
+}
+
+func countOverlaps(in line: Line, on grid: inout [[Int]]) -> Int {
+   var overlapped = 0
 
    func markGridPosition(row: Int, column: Int) {
       if grid[row][column] == 1 {
@@ -61,30 +76,28 @@ func partOne(_ lines: [Line]) -> Int {
       }
       grid[row][column] += 1
    }
-
-   func order(_ line: Line) -> (start: Point, end: Point) {
-      line.from.x < line.to.x || line.from.y < line.to.y ? (start: line.from, end: line.to) : (start: line.to, end: line.from)
+   func calcIncrementor(_ from: Int, _ to: Int) -> Int {
+      if from == to { return 0} else {
+         if to > from { return 1 } else {
+            return -1
+         }
+      }
    }
-
-   for line in lines where line.from.x == line.to.x || line.from.y == line.to.y {
-      // get orderd pair of start/end points, such that start point can
-      // be iterated on up to end point.
-      let (start, end) = order(line)
-      // plot line on grid
-      let addColumn = line.from.x == line.to.x ? 0 : 1
-      let addRow = line.from.y == line.to.y ? 0 : 1
-      var current = start
-      repeat {
-         markGridPosition( row: current.y, column: current.x )
-         current.x += addColumn
-         current.y += addRow
-      } while (current.x <= end.x && current.y <= end.y)
+   func pointIsOnLine() -> Bool {
+      current.x.isBetween(line.from.x, line.to.x) &&
+      current.y.isBetween(line.from.y, line.to.y )
    }
-//   print("Final grid")
-//   for row in grid { print(row.reduce("") { j,i in i==0 ? j+"." : j+String(i)}) }
+   // plot line on grid
+   var current = line.from
+   let columnIncrement = calcIncrementor(line.from.x, line.to.x)
+   let rowIncrement = calcIncrementor(line.from.y, line.to.y)
+   repeat {
+      markGridPosition( row: current.y, column: current.x )
+      current.x += columnIncrement
+      current.y += rowIncrement
+   } while (pointIsOnLine() )
 
    return overlapped
-
 }
 
 func partTwo(_ lines: [Line]) -> Int {
@@ -93,49 +106,12 @@ func partTwo(_ lines: [Line]) -> Int {
    let row = Array(repeating: 0, count: arraySize)
    var grid = Array(repeating: row, count: arraySize)
 
-   func markGridPosition(row: Int, column: Int) {
-      if grid[row][column] == 1 {
-         overlapped += 1
-      }
-      grid[row][column] += 1
-   }
-
    for line in lines {
-      print("from \(line.from.x),\(line.from.y) to \(line.to.x),\(line.to.y)")
-      var columnIncrement = 0
-      var rowIncrement = 0
-
-      // plot line on grid
-      if line.from.x == line.to.x { columnIncrement = 0} else {
-         if line.to.x > line.from.x { columnIncrement = 1 } else {
-            columnIncrement = -1
-         }
-      }
-      if line.from.y == line.to.y { rowIncrement = 0} else {
-         if line.to.y > line.from.y { rowIncrement = 1 } else {
-            rowIncrement = -1
-         }
-      }
-      print(" starting at \(line.from.x), \(line.from.y) ")
-      print("adding to columns: \(columnIncrement)")
-      print("adding to rows: \(rowIncrement)")
-
-      func pointIsOnLine() -> Bool {
-         current.x.isBetween(line.from.x, line.to.x) &&
-         current.y.isBetween(line.from.y, line.to.y )
-      }
-      var current = line.from
-      repeat {
-         markGridPosition( row: current.y, column: current.x )
-         current.x += columnIncrement
-         current.y += rowIncrement
-      } while (pointIsOnLine() )
+ 		overlapped += countOverlaps(in: line, on: &grid)
    }
-      print("Final grid")
-      for row in grid { print(row.reduce("") { j,i in i==0 ? j+"." : j+String(i)}) }
-
+   print("\nPart Two")
+   printGrid(grid)
    return overlapped
-
 }
 
 extension Comparable  {
